e45ab2be1bdc94e9c810fd3aa5acb5a40a046ce6
ksh-fthr <ksh.fthr0806@gmail.com>
2018-04-15 15:52:48 +0900

    ディレクティブに渡すパラメータをプロパティバインディングから直接文字列をセットする方針に変更｡

--- src/app/component/use-directive/use-directive.component.ts
+++ src/app/component/use-directive/use-directive.component.ts
@@ -7,23 +7,6 @@ import { TemplateDirective } from '../../directive/template.directive';
   styleUrls: ['./use-directive.component.css']
 })
 export class UseDirectiveComponent implements OnInit {
-
-  /**
-   * 本コンポーネントの html テンプレートでセットしているディレクティブに渡すデータ-1
-   *
-   * @type {string}
-   * @memberof UseDirectiveComponent
-   */
-  public greet: string;
-
-  /**
-   * 本コンポーネントの html テンプレートでセットしているディレクティブに渡すデータ-2
-   *
-   * @type {string}
-   * @memberof UseDirectiveComponent
-   */
-  public name: string;
-
   /**
    * コンストラクタ ( 本コンポーネントではなにもしない )
    *
@@ -32,15 +15,9 @@ export class UseDirectiveComponent implements OnInit {
   constructor() { }
 
   /**
-   * 初期処理
-   * プロパティ: inputData に文字列をセット
-   * ここでセットされた文字列がディレクティブに渡り､ブラウザ上に表示される
+   * 初期処理 ( 本コンポーネントではなにもしない )
    *
    * @memberof UseDirectiveComponent
    */
-  ngOnInit() {
-    this.greet = 'Hello';
-    this.name = 'Angular';
-  }
-
+  ngOnInit() { }
 }
