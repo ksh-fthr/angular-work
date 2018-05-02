1922b69297a4c16bbe8a997c58a16d63f51905ef
ksh-fthr <ksh.fthr0806@gmail.com>
2018-05-02 14:01:45 +0900

    不要コードの削除

--- src/app/directive/network-address-validator.directive.ts
+++ src/app/directive/network-address-validator.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, Input } from '@angular/core';
+import { Directive } from '@angular/core';
 import { AbstractControl, NG_VALIDATORS, Validator } from '@angular/forms';
 
 @Directive({
