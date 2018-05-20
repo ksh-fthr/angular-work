c747f18b700e81bc872f73372b1b7a1791212ed6
ksh-fthr <ksh.fthr0806@gmail.com>
2018-05-20 18:21:00 +0900

    feat_validation で対応した最後の入力項目に対するバリデーションエラーのみ表示する実装こちらにも適用した

--- src/app/component/use-directive/use-directive.component.ts
+++ src/app/component/use-directive/use-directive.component.ts
@@ -16,6 +16,31 @@ export class UseDirectiveComponent implements OnInit {
   public inputAddress: string;
 
   /**
+   * 入力されたアドレス
+   *
+   * @type {string}
+   * @memberof UseDirectiveComponent
+   */
+  public inputSubnetMask: string;
+
+  /**
+   * 入力エラー情報を画面に表示するためのプロパティ
+   *
+   * @type {*}
+   * @memberof ValidationComponent
+   */
+  public validationError: any;
+
+  /**
+   * 入力エラー情報を管理するためのリスト
+   *
+   * @private
+   * @type {*}
+   * @memberof ValidationComponent
+   */
+  private validationErrorList: any = [];
+
+  /**
    * コンストラクタ ( 本コンポーネントではなにもしない )
    *
    * @memberof UseDirectiveComponent
@@ -38,5 +63,59 @@ export class UseDirectiveComponent implements OnInit {
    */
   public onClickOK($event) {
     alert('OK button had clicked.');
+  }
+
+
+  /**
+   * keyup イベントのイベントハンドラ
+   * このイベントをトリガーに入力エラー情報を管理する
+   *
+   * @param {any} validationKey
+   * @param {any} errorInformation
+   * @memberof ValidationComponent
+   */
+  public onKeyUp(validationKey, errorInformation) {
+    this.manageValidationError(validationKey, errorInformation)
+  }
+
+  /**
+   * 入力エラー情報を管理する
+   * 具体的には次の処理を行う
+   * # 引数の validationKey を元にエラー情報を一意に管理する
+   * # ビューに表示するためのエラー情報にリストの最後の情報をセットする
+   *
+   * @private
+   * @param {any} validationKey エラーが発生した入力フォーム
+   * @param {any} errorInformation バリデーションエラー情報
+   * @memberof ValidationComponent
+   */
+  private manageValidationError(validationKey, errorInformation) {
+    if (errorInformation) {
+      let isExist: boolean = false;
+      for (const target in this.validationErrorList) {
+        if (this.validationErrorList.hasOwnProperty(target) &&
+            this.validationErrorList[target].key === validationKey) {
+          isExist = true;
+          this.validationErrorList.splice(target, 1);
+          this.validationErrorList.push({ key: validationKey, error: errorInformation });
+          break;
+        }
+      }
+
+      if (!isExist) {
+        this.validationErrorList.push({ key: validationKey, error: errorInformation });
+      }
+    } else {
+      for (const target in this.validationErrorList) {
+        if (this.validationErrorList.hasOwnProperty(target) &&
+            this.validationErrorList[target].key === validationKey) {
+          this.validationErrorList.splice(target, 1);
+          break;
+        }
+      }
+    }
+
+    const errorData: any = this.validationErrorList[this.validationErrorList.length - 1];
+    this.validationError = errorData ? errorData.error : undefined;
   }
 }
