9853b0fd1231c531a7800de8717f5720a1434f3c
ksh-fthr <ksh.fthr0806@gmail.com>
2018-05-26 23:21:50 +0900

    不要コードの削除

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,5 @@
 import { Component, AfterContentInit, AfterContentChecked, ContentChild } from '@angular/core';
 
-import { HogeService } from './service/hoge.service';
-import { Bar1Service } from './service/bar1.service';
-import { Bar2Service } from './service/bar2.service';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -13,11 +9,6 @@ export class AppComponent {
   title = 'app';
 
   constructor(
-    private hogeService: HogeService,
-    private bar1Service: Bar1Service,
-    private bar2Service: Bar2Service
   ) {
-    hogeService.exec(bar1Service);
-    hogeService.exec(bar2Service);
   }
 }
