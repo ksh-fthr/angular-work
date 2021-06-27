5fe9370b09853677af6109c5b52aa17ef7d4a8db
ksh-fthr <ksh.fthr0806@gmail.com>
2021-06-27 17:35:50 +0900

    WIP, 一旦コミットしておく

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,47 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
-
-import { ModalComponent } from './component/modal/modal.component';
+import { PageAComponent } from './component/routing/page-a/page-a.component';
+import { PageBComponent } from './component/routing/page-b/page-b.component';
+import { PageCComponent } from './component/routing/page-c/page-c.component';
+import { ModalBaseComponent } from './component/modal/modal-base.component';
+import { ModalComponent } from './component/modal/modal/modal.component';
+import { RoutingComponent } from './component/routing/routing.component';
 
 import { ModalService } from './service/modal.service';
+
+
+// Routing を行う対象のコンポーネントを管理する
+// path にセットした文字列にマッチしたURLが指定されると、対になっているコンポーネントが表示される
+// 下記のように明示する以外にも
+//    '' で [/] のルートパスを指定できる
+//    '*＊' でワイルドカードを指定できる
+const ROUTE_TABLE: Routes = [
+  { path: 'modal', component: ModalComponent },
+  { path: 'routing', component: RoutingComponent },
+  { path: 'routing/page-a', component: PageAComponent },
+  { path: 'routing/page-b', component: PageBComponent },
+  { path: 'routing/page-c', component: PageCComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
+    PageAComponent,
+    PageBComponent,
+    PageCComponent,
     ModalComponent,
+    RoutingComponent,
+    ModalBaseComponent,
   ],
   entryComponents: [
     ModalComponent,
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot(ROUTE_TABLE), // 追加. routing の情報を登録する
   ],
   providers: [
     ModalService
---
+++ src/app/component/routing/page-a/page-a.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-page-a',
+  templateUrl: './page-a.component.html',
+  styleUrls: ['./page-a.component.css']
+})
+export class PageAComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+}
