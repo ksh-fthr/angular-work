45f91fe5f50c9206e1c7cab6fc5fe5954806fe4a
ksh-fthr <ksh.fthr0806@gmail.com>
2021-06-29 00:12:27 +0900

    feat_switch_tab で実装した内容をマージ.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+
+// ルーティング実装検証用コンポーネント/サービス
+import { RoutingComponent } from './component/routing/routing.component';
 import { PageAComponent } from './component/routing/page-a/page-a.component';
 import { PageBComponent } from './component/routing/page-b/page-b.component';
 import { PageCComponent } from './component/routing/page-c/page-c.component';
+
+// モーダル実装検証用コンポーネント/サービス
 import { ModalBaseComponent } from './component/modal/modal-base.component';
 import { ModalComponent } from './component/modal/modal/modal.component';
-import { RoutingComponent } from './component/routing/routing.component';
+import { SwitchTabComponent } from './component/tab/switch-tab/switch-tab.component';
+import { ModalService } from './service/modal/modal.service';
 
-import { ModalService } from './service/modal.service';
+// タブ実装検証用コンポーネント/サービス
+import { TabBaseComponent } from './component/tab/tab-base.component';
+import { TabAComponent } from './component/tab/tab-a/tab-a.component';
+import { TabBComponent } from './component/tab/tab-b/tab-b.component';
+import { SwitchTabService } from './service/switch-tab/switch-tab.service';
 
 
 // Routing を行う対象のコンポーネントを管理する
@@ -19,10 +29,6 @@ import { ModalService } from './service/modal.service';
 //    '' で [/] のルートパスを指定できる
 //    '*＊' でワイルドカードを指定できる
 const ROUTE_TABLE: Routes = [
-  {
-    path: 'modal',
-    component: ModalBaseComponent
-  },
   {
     path: 'routing',
     component: RoutingComponent,
@@ -33,17 +39,32 @@ const ROUTE_TABLE: Routes = [
       { path: 'page-c', component: PageCComponent }
     ]
   },
+  {
+    path: 'modal',
+    component: ModalBaseComponent
+  },
+  {
+    path: 'tab',
+    component: TabBaseComponent
+  },
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
+    // ルーティング実装検証用コンポーネント
+    RoutingComponent,
     PageAComponent,
     PageBComponent,
     PageCComponent,
+    // モーダル実装検証用コンポーネント
     ModalComponent,
-    RoutingComponent,
     ModalBaseComponent,
+    // タブ実装検証用コンポーネント
+    TabBaseComponent,
+    SwitchTabComponent,
+    TabAComponent,
+    TabBComponent,
   ],
   entryComponents: [
     ModalComponent,
@@ -53,7 +74,8 @@ const ROUTE_TABLE: Routes = [
     RouterModule.forRoot(ROUTE_TABLE), // 追加. routing の情報を登録する
   ],
   providers: [
-    ModalService
+    ModalService,
+    SwitchTabService
   ],
   bootstrap: [AppComponent]
 })
