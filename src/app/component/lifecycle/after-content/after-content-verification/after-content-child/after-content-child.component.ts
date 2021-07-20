07dac8aa53017197d322b0d1ee1ec1b352c81d69
ksh-fthr <ksh.fthr0806@gmail.com>
2021-07-20 10:47:51 +0900

    画面上へのログ出力処理の実装とコンポーネントのパス変更等の軽いリファクタリング.

---
+++ src/app/component/lifecycle/after-content/after-content-verification/after-content-child/after-content-child.component.ts
@@ -0,0 +1,35 @@
+import { Component, ElementRef, OnInit } from '@angular/core';
+import { Logging } from '../../../../../utils/logging';
+
+@Component({
+  selector: 'app-after-content-child',
+  templateUrl: './after-content-child.component.html',
+  styleUrls: ['./after-content-child.component.css']
+})
+export class AfterContentChildComponent implements OnInit {
+  // 親コンポーネントに渡す値をセットする変数
+  public childContents: string = '';
+
+  /**
+   * ログ出力を行うテキストエリアの HTML エレメント
+   */
+   private textAreaElement: any;
+
+   constructor(
+    private element: ElementRef,
+   ) {
+    // コンストラクタは画面描画の前に実行されるので画面上の要素を取得することができない
+    // よって､ここではコンソールログに出力するだけになる
+    console.log('[AfterContentChildComponent][constructor] fired');
+  }
+
+  ngOnInit(): void {
+    this.childContents = '子コンポーネント で初期化しました。';
+    const message: string = `[AfterContentChildComponent][ngOnInit] fired. childContents={ ${this.childContents} }`;
+    console.log(message);
+
+    this.textAreaElement = this.element.nativeElement.querySelector('#log-text-area-child');
+    Logging.info(this.textAreaElement, message);
+  }
+
+}
---
+++ src/app/component/lifecycle/after-content/after-content-verification/after-content-parent/after-content-parent.component.ts
@@ -0,0 +1,64 @@
+import { Component, OnInit, ContentChild, ElementRef } from '@angular/core';
+import { AfterContentChildComponent } from '../after-content-child/after-content-child.component';
+import { Logging } from '../../../../../utils/logging';
+
+@Component({
+  selector: 'app-after-content-parent',
+  templateUrl: './after-content-parent.component.html',
+  styleUrls: ['./after-content-parent.component.css']
+})
+export class AfterContentParentComponent implements OnInit {
+
+  // 外部コンテンツである子コンポーネントを参照
+  @ContentChild(AfterContentChildComponent) child!: AfterContentChildComponent;
+
+  // 子コンポーネントのパラメータの値をセットする変数
+  private parentContents: string = '';
+
+  /**
+   * ログ出力を行うテキストエリアの HTML エレメント
+   */
+   private textAreaElement: any;
+
+  constructor(
+    private element: ElementRef,
+  ) {
+    // コンストラクタは画面描画の前に実行されるので画面上の要素を取得することができない
+    // よって､ここではコンソールログに出力するだけになる
+    console.log('[AfterContentParentComponent][constructor] fired');
+  }
+
+  /**
+   * コンポーネントの初期化
+   */
+  ngOnInit() {
+    // 画面上のログは外部コンテンツのエリアに出力する
+    this.textAreaElement = this.element.nativeElement.querySelector('#log-text-area-child');
+
+    const message: string = '[AfterContentParentComponent][ngOnInit] fired';
+    console.log(message);
+    Logging.info(this.textAreaElement, message);
+  }
+
+  /**
+   * 外部コンテンツが初期化された後に処理
+   */
+  ngAfterContentInit() {
+    this.parentContents = this.child.childContents;
+
+    const message: string = `[AfterContentParentComponent][ngAfterContentInit] fired. parentContents={ ${this.parentContents} }`;
+    console.log(message);
+    Logging.info(this.textAreaElement, message);
+  }
+
+  /**
+   * 外部コンテンツの確認後に処理
+   */
+  ngAfterContentChecked() {
+    this.parentContents = this.child.childContents;
+
+    const message: string = `[AfterContentParentComponent][ngAfterContentChecked] fired. parentContents={ ${this.parentContents} }`;
+    console.log(message);
+    Logging.info(this.textAreaElement, message);
+  }
+}
