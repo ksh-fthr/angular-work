ee6b9aa16349aa5fcbbc14c1149c8a049c833f89
ksh-fthr <ksh.fthr0806@gmail.com>
2021-07-31 17:24:22 +0900

    feat_validation で実装した内容をマージ.

---
+++ src/app/component/validation/validation-base.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ValidationBaseComponent } from './validation-base.component';
+
+describe('ValidationBaseComponent', () => {
+  let component: ValidationBaseComponent;
+  let fixture: ComponentFixture<ValidationBaseComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ValidationBaseComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ValidationBaseComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
