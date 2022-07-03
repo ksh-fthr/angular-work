9fee778859a3d1380ad77ff9514a2fc9bd99f172
ksh-fthr <ksh.fthr0806@gmail.com>
2022-07-03 13:08:09 +0900

    親子関係にあるコンポーネント間でのデータ授受の検証用実装をモジュールに切り出した.

---
+++ src/app/modules/feature-parent-child.modules.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+// 親子関係にあるコンポーネント間でのデータ受け渡しの検証用コンポーネント
+import { ParentChildBaseComponent } from '../component/parent-child/parent-child-base.component';
+import { ParentComponent } from '../component/parent-child/parent/parent.component';
+import { ChildComponent } from '../component/parent-child/child/child.component';
+
+@NgModule({
+  declarations: [ParentChildBaseComponent, ParentComponent, ChildComponent],
+  imports: [CommonModule, FormsModule],
+  exports: [ParentChildBaseComponent, ParentComponent, ChildComponent],
+})
+export class FeatureParentChildModule {}
