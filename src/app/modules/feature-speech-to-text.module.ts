6a704eaf51c5d5803b0ce38045d2bc8103ea640b
ksh-fthr <ksh.fthr0806@gmail.com>
2024-06-15 19:12:24 +0900

    `Dynamic require of "microphone-stream" is not supported` が解決しないので、一旦該当するコンポーネントアウトしてビルド対象外とする.

--- src/app/modules/feature-speech-to-text.module.ts
+++ src/app/modules/feature-speech-to-text.module.ts
@@ -4,7 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 // 音声認識-文字起こしの検証用コンポーネント
 import { SpeechToTextBaseComponent } from '../component/speech-to-text/speech-to-text-base.component';
 import { UseWebSpeechApiComponent } from '../component/speech-to-text/use-web-speech-api/use-web-speech-api.component';
-import { UseAwsTranscribeStreamingComponent } from '../component/speech-to-text/use-aws-transcribe-streaming/use-aws-transcribe-streaming.component';
+// import { UseAwsTranscribeStreamingComponent } from '../component/speech-to-text/use-aws-transcribe-streaming/use-aws-transcribe-streaming.component';
 
 const ROUTE_TABLE: Routes = [
   {
@@ -12,13 +12,22 @@ const ROUTE_TABLE: Routes = [
     component: SpeechToTextBaseComponent,
     children: [
       { path: 'use-web-speech-api', component: UseWebSpeechApiComponent },
-      { path: 'use-aws-transcribe', component: UseAwsTranscribeStreamingComponent },
+      // { path: 'use-aws-transcribe', component: UseAwsTranscribeStreamingComponent },
     ],
   },
 ];
 @NgModule({
-  declarations: [SpeechToTextBaseComponent, UseWebSpeechApiComponent, UseAwsTranscribeStreamingComponent],
+  declarations: [
+    SpeechToTextBaseComponent,
+    UseWebSpeechApiComponent,
+    // UseAwsTranscribeStreamingComponent
+  ],
   imports: [RouterModule.forRoot(ROUTE_TABLE)],
-  exports: [SpeechToTextBaseComponent, UseWebSpeechApiComponent, UseAwsTranscribeStreamingComponent, RouterModule],
+  exports: [
+    SpeechToTextBaseComponent,
+    UseWebSpeechApiComponent,
+    // UseAwsTranscribeStreamingComponent,
+    RouterModule,
+  ],
 })
 export class FeatureSpeechToTextModule {}
