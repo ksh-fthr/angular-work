681c449a5a9e2375ea9971780495020bcad26af5
ksh-fthr <ksh.fthr0806@gmail.com>
2025-09-11 09:40:56 +0900

    不要な import を削除.

--- src/app/modules/feature-design-pattern.module.ts
+++ src/app/modules/feature-design-pattern.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule } from '@angular/router';
 import { DesignPatternBaseComponent } from '../component/design-pattern/design-pattern-base.component';
 import { StatePatternClientComponent } from '../component/design-pattern/state/state-pattern-client.component';
 
